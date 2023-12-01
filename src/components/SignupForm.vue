d1bdbf7a5b86d11e0f6d3c0a483f40ade378acbc
faten282 <112074192+fatenhussein@users.noreply.github.com>
2023-12-01 21:42:20 +0300

    first commit

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,15 @@
+<template lang="">
+<form >
+<label > Email:</label>
+<input type="email" required>
+
+</form>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
