b511bf045fa16e2f3b8e97fb4de7b2df9f1f292a
faten282 <112074192+fatenhussein@users.noreply.github.com>
2023-12-02 14:59:03 +0300

    first commit

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -5,17 +5,39 @@
 
     <label> Password:</label>
     <input type="password" required v-model="pass" />
+
+    <label>Role:</label>
+
+    <select v-model="role">
+      <option value="developer">web developer</option>
+      <option value="designer">web designer</option>
+    </select>
+
+
+
+
+    <div class="terms">
+        <input type="checkbox" v-model='terms'>
+
+        <label > accept terms and conditions </label>
+
+    </div>
   </form>
 
   <p>email:{{ email }}</p>
   <p>pass:{{ pass }}</p>
+  <p>role:{{ role }}</p>
+
+  <p>terms accsepted :{{terms}}</p>
 </template>
 <script>
 export default {
   data() {
     return {
-      email: '',
+      email: 'hhh',
       pass: '',
+      role: 'designer',
+      terms: false
     };
   },
 };
